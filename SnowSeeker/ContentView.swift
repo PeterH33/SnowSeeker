ff242a278a573f93971452a2701788599aae4a31
Peter Hartnett <peter.hartnett@gmail.com>
2022-05-21 11:48:41 -0400

    Initial Commit

---
+++ SnowSeeker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SnowSeeker
+//
+//  Created by Peter Hartnett on 5/21/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Peter Hartnett on 5/21/22.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
